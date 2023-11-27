b70c289fd24fff386d286d18a4bf480668d48a38
Michal Hoftich <michal.h21@gmail.com>
2023-11-27 15:57:08 +0100

    progress

--- make_man.lua
+++ make_man.lua
@@ -3,6 +3,10 @@ _G.__test__ = true
 -- try to translate the help message to a Man page
 local texblend = require("texblend")
 
+local function parse_sections(text)
+  -- 
+  local sections = {}
+end
 
 
 print(texblend.cmd_options)
